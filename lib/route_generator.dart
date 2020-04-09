ea6e79f740c44dcfc03ee5efdf7d2de450b7eaf6
Robert Stevens <robert.s@wigroup.co.za>
2020-04-09 14:16:53 +0200

    added forums route

--- lib/route_generator.dart
+++ lib/route_generator.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:shareLearnTeach/src/models/route_argument.dart';
+// import 'package:shareLearnTeach/src/models/topic.dart';
 import 'package:shareLearnTeach/src/screens/Categorie.dart';
 import 'package:shareLearnTeach/src/screens/account.dart';
 import 'package:shareLearnTeach/src/screens/Categories.dart';
 import 'package:shareLearnTeach/src/screens/Resources.dart';
-import 'package:shareLearnTeach/src/screens/favorites.dart';
+import 'package:shareLearnTeach/src/screens/forums/index.dart';
+// import 'package:shareLearnTeach/src/screens/forums/topic.dart';
+// import 'package:shareLearnTeach/src/screens/forums/job_bulletin.dart';
+// import 'package:shareLearnTeach/src/screens/favorites.dart';
 import 'package:shareLearnTeach/src/screens/languages.dart';
-import 'package:shareLearnTeach/src/screens/on_boarding.dart';
+// import 'package:shareLearnTeach/src/screens/on_boarding.dart';
 import 'package:shareLearnTeach/src/screens/signin.dart';
 import 'package:shareLearnTeach/src/screens/signup.dart';
-import 'package:shareLearnTeach/src/screens/splashScreen.dart';
+// import 'package:shareLearnTeach/src/screens/splashScreen.dart';
 import 'package:shareLearnTeach/src/screens/tabs.dart';
 import 'package:shareLearnTeach/src/screens/utilitie.dart';
 
@@ -29,7 +33,7 @@ class RouteGenerator {
       case '/Account':
         return MaterialPageRoute<dynamic>(builder: (_) => AccountWidget()); 
       case '/Tabs':
-        return MaterialPageRoute<dynamic>(builder: (_) => TabsWidget(currentTab: args,));    
+        return MaterialPageRoute<dynamic>(builder: (_) => TabsWidget(currentTab: args as int,));    
       case '/Utilities':
         return MaterialPageRoute<dynamic>(builder: (_) => UtilitieWidget(routeArgument: args as RouteArgument,));
       case '/Languages':
@@ -40,6 +44,12 @@ class RouteGenerator {
       //   return MaterialPageRoute(builder: (_) => FavoritesWidget());
       case '/Resources':
         return MaterialPageRoute<dynamic>(builder: (_) => ResourcesWidget());
+      case '/Forums':
+        return MaterialPageRoute<dynamic>(builder: (_) => ForumsScreen());
+      // case '/Topic':
+      //   return MaterialPageRoute<dynamic>(builder: (_) => TopicScreen());
+      // case '/Jobs':
+      //   return MaterialPageRoute<dynamic>(builder: (_) => const JobBulletinScreen());
       case '/Categorie':
         return MaterialPageRoute<dynamic>(builder: (_) => CategorieWidget(routeArgument: args as  RouteArgument,));
 
